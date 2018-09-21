00a270063ec4355915f339fd8ee933f8dfc1352d
Joakim Rosenqvist <jr222wb@student.lnu.se>
2018-09-21 15:08:49 +0200

    Lagt till hantering av båtar

--- src/model/TestReg.java
+++ src/model/TestReg.java
@@ -1,6 +1,10 @@
 package model;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+
+import model.Boat.BoatType;
 
 public class TestReg {
     private static Scanner input = new Scanner(System.in);
@@ -17,6 +21,9 @@ public class TestReg {
         System.out.println("4. Edit member name");
         System.out.println("5. Edit member PNR");
         System.out.println("6. Print all members");
+        System.out.println("7. Register boat");
+        System.out.println("8. Remove boat");
+        System.out.println("9. List boats");
         System.out.print("Choice: ");
         int choice = Integer.parseInt(input.nextLine());
 
@@ -39,13 +46,71 @@ public class TestReg {
         case 6:
             getAll();
             break;
+        case 7:
+        	registerBoat();
+        	break;
+        case 8:
+        	removeBoat();
+        	break;
+        case 9: 
+        	listBoats();
+        	break;
         default:
             System.out.println("Invalid");
             break;
         }
     }
 
-    private static void getAll() {
+    private static void listBoats() {
+    	System.out.print("Enter id for member: ");
+		int id = Integer.parseInt(input.nextLine());
+		for (Member m : mr.getAllMembers()) {
+			if (m.getId() == id) {
+				for (Boat b : m.getBoats()) {
+					System.out.println(b.getType() + " " + b.getSize());
+				}
+			}
+		}
+		displayMenu();
+	}
+
+	private static void removeBoat() {    	
+		System.out.print("Enter id for member: ");
+		int id = Integer.parseInt(input.nextLine());
+		for (Member m : mr.getAllMembers()) {
+			if (m.getId() == id) {
+				for (Boat b : m.getBoats()) {
+					System.out.println(b.getType() + " " + b.getSize());
+				}
+				System.out.print("Enter index of boat to remove: ");
+				id = Integer.parseInt(input.nextLine());
+				m.removeBoat(id);
+			}
+		}
+		displayMenu();		
+	}
+
+	private static void registerBoat() {
+		List<BoatType> types = Arrays.asList(BoatType.values());
+	
+		System.out.print("Enter id for member: ");
+		int id = Integer.parseInt(input.nextLine());
+		for (Member m : mr.getAllMembers()) {
+			if (m.getId() == id) {
+				for (BoatType t : types) {
+					System.out.println(t);
+				}
+				System.out.print("Enter type: ");
+				BoatType type = Enum.valueOf(BoatType.class, input.nextLine());
+				System.out.print("Enter size: ");
+				double size = Float.parseFloat(input.nextLine());
+				m.registerBoat(type, size);
+			}
+		}
+		displayMenu();
+	}
+
+	private static void getAll() {
     	for (Member m : mr.getAllMembers())
             System.out.println("id " + m.getId() + ":\tname: " + m.getName() + "\tpnr: " + m.getPNr());
     	displayMenu();
