7d6cedfbb1a4e40ef8271d0a1fba0fb15347b7c4
Ninsua <e@anjel.se>
2018-10-08 14:20:51 +0200

    Removed the usage of a super class. It will be implemented in grade 3/4.

--- src/Program.java
+++ src/Program.java
@@ -5,7 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		model.MemberRegistry regM = new model.MemberRegistry();
 		view.ViewInterface view = new view.EngConsole();
-		controller.User controller = new Admin(regM, view);
+		controller.Admin controller = new Admin(regM, view);
 		
 		controller.startApplication();
 
