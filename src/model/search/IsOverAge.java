eb992a8682698a97d9c82538e9c80fb86102f404
Joakim Rosenqvist <jr222wb@student.lnu.se>
2018-10-23 15:00:15 +0200

    Ändrat från Date till Calendar

--- src/model/search/IsOverAge.java
+++ src/model/search/IsOverAge.java
@@ -2,16 +2,17 @@ package model.search;
 
 import model.Member;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Calendar;
 
 class IsOverAge implements ISearchStrategy {
 
     public ArrayList<Member> returnFiltered(ArrayList<Member> members, String searchParameter) {
         ArrayList<Member> membersFiltered = new ArrayList<Member>();
+        Calendar cal = Calendar.getInstance();
 
         for(Member member : members) {
             int memberBirthYear = Integer.parseInt(member.getPNr().substring(0, 4));
-            int currentYear = new Date().getYear() + 1900;
+            int currentYear = cal.get(Calendar.YEAR);
             String memberAge = "" + (currentYear - memberBirthYear);
             if(memberAge.compareTo(searchParameter) == 1) {
                 membersFiltered.add(member);
